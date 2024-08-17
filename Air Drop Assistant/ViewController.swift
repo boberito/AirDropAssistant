0e71261c49057b6967186255946c374994c79dea
Bob Gendler <boberito@mac.com>
2024-08-16 20:06:37 -0400

    Initial Commit

---
+++ Air Drop Assistant/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Air Drop Assistant
+//
+//  Created by Bob Gendler on 8/16/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
