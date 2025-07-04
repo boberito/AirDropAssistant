db7734fd273999cd9e877b79da6e55850ab9e84e
Bob Gendler <boberito@mac.com>
2025-07-03 22:43:58 -0400

    removed old method for update check

--- Air Drop Assistant/UpdateCheck.swift
+++ Air Drop Assistant/UpdateCheck.swift
@@ -47,56 +47,6 @@ class UpdateCheck {
                     }
                 }
         }
-//        return updateNeeded
-//        let dispatchGroup = DispatchGroup()
-//        dispatchGroup.enter()
-//        var version: String? = nil
-//        var updateNeeded = 0
-//        let session = URLSession.shared
-//        let task = session.dataTask(with: request as URLRequest) {data,response,error in
-//            let httpResponse = response as? HTTPURLResponse
-//            let dataReturn = data
-//            if (error != nil) {
-//                Logger.updater.error("An Error Occured - offline or can't reach GitHub")
-//                updateNeeded = 2
-//                dispatchGroup.leave()
-//            } else {
-//                do {
-//                    switch httpResponse!.statusCode {
-//                    case 200:
-//                        let decoder = JSONDecoder()
-//                        if let githubData = try? decoder.decode(githubData.self, from: dataReturn!) {
-//                            version = githubData.tag_name
-//                            if let currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String, let gitHubVersion = version {
-//                                let versionCompare = currentVersion.compare(gitHubVersion, options: .numeric)
-//                                if versionCompare == .orderedSame {
-//                                    Logger.updater.info("ADA is update to date")
-//                                    updateNeeded = 0
-//                                } else if versionCompare == .orderedAscending {
-//                                    DispatchQueue.main.async {
-//                                        self.alert(githubVersion: gitHubVersion, current: currentVersion)
-//                                    }
-//                                    Logger.updater.info("Current is \(currentVersion.description), newest is \(gitHubVersion.description)")
-//                                    updateNeeded = 1
-//                                } else if versionCompare == .orderedDescending {
-//                                    Logger.updater.info("Current is \(currentVersion.description), newest is \(gitHubVersion.description)")
-//                                    updateNeeded = 0
-//                                }
-//                            }
-//                        }
-//                        dispatchGroup.leave()
-//                    default:
-//                        Logger.updater.error("Offline or cannot reach GitHub")
-//                        updateNeeded = 2
-//                        dispatchGroup.leave()
-//                    }
-//                }
-//            }
-//        }
-//        task.resume()
-//        dispatchGroup.wait()
-//        
-//        return updateNeeded
     }
     
     func alert(githubVersion: String, current: String) {
